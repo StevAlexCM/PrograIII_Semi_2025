3a0f34087615cbd369d8d13f466a87aaf6910499
StevAlexCM <campossteven593@gmail.com>
2025-09-23 00:25:42 -0600

    CRUD

--- server.py
+++ server.py
@@ -1,27 +1,30 @@
-from http.server import SimpleHTTPRequestHandler, HTTPServer
+from http.server import HTTPServer, SimpleHTTPRequestHandler
 from urllib import parse
-port=3000
+import json 
+import crud_alumno
+
+port = 3000
+
+crudAlumno = crud_alumno.crud_alumno()
+
 class miServidor(SimpleHTTPRequestHandler):
     def do_GET(self):
         if self.path=="/":
             self.path="index.html"
             return SimpleHTTPRequestHandler.do_GET(self)
-        
-        
-        
-    def do_post(self):
+    
+    def do_POST(self):
         longitud = int(self.headers['Content-Length'])
         datos = self.rfile.read(longitud)
         datos = datos.decode("utf-8")
         datos = parse.unquote(datos)
+        datos = json.loads(datos)
+        resp = {"msg": crudAlumno.administrar(datos)}
         
         self.send_response(200)
-        self.send_header()
-        self.wfile.write
-        print(datos)
-        
-        
-print("Ejecutando el servidor en el puerto", port)
+        self.end_headers()
+        self.wfile.write(json.dumps(resp).encode("utf-8"))
+
+print("Servidor ejecutandose en el puerto", port)
 server = HTTPServer(("localhost", port), miServidor)
 server.serve_forever()
-    
